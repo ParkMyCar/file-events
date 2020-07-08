7d75676fc1460d6a5f7ed957267c3a404f82086e
Parker Timmerman <parker@parkertimmerman.com>
2020-07-08 15:35:11 -0400

    initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,121 @@
+use core_foundation::{
+    array::CFArray,
+    base::{
+        ItemMutRef,
+        FromMutVoid,
+        TCFType,
+    },
+    string::CFString,
+    self as cf,
+};
+use crossbeam_channel::{
+    self as channel,
+    Receiver,
+    Sender,
+};
+use fsevent_sys as fsevent;
+
+use file_events::{
+    FSEventStreamCreate,
+    FSEventStreamScheduleWithRunLoop,
+    FSEventStreamStart,
+};
+
+use std::{
+    ffi,
+    mem::MaybeUninit,
+    ptr,
+    sync::Once,
+    time::Duration,
+};
+
+static mut CHANNEL: MaybeUninit<(Sender<Option<String>>, Receiver<Option<String>>)> = MaybeUninit::uninit();
+static CHANNEL_INIT: Once = Once::new();
+
+#[inline]
+fn get_channel() -> &'static (Sender<Option<String>>, Receiver<Option<String>>) {
+    CHANNEL_INIT.call_once(|| unsafe {
+        ptr::write(CHANNEL.as_mut_ptr(), channel::unbounded());
+    });
+
+    unsafe {
+        &*CHANNEL.as_ptr()
+    }
+}
+
+fn main() {
+    std::thread::spawn(|| {
+        let stream_flags = fsevent::kFSEventStreamCreateFlagUseCFTypes
+            | fsevent::kFSEventStreamCreateFlagFileEvents;
+
+        let paths: Vec<CFString> = vec![CFString::from_static_string("/Users/parkmycar/TestFileEvents")];
+        println!("Watching paths:\n{:#?}", &paths);
+
+        let cf_paths = CFArray::from_CFTypes(&paths[..]);
+
+        let stream_ref = unsafe {
+            println!("creating stream!");
+            let stream_ref = FSEventStreamCreate(
+                fsevent::core_foundation::kCFAllocatorDefault,
+                Some(callback),
+                ptr::null_mut(),
+                cf_paths.as_concrete_TypeRef(),
+                fsevent::kFSEventStreamEventIdSinceNow,
+                Duration::from_secs(1).as_secs_f64(),
+                stream_flags);
+    
+            if stream_ref.is_null() {
+                panic!("Failed to create FS Event Stream");
+            }
+    
+            stream_ref
+        };
+        println!("stream created!");
+
+        unsafe {
+            println!("shceduling with runloop!");
+            FSEventStreamScheduleWithRunLoop(
+                stream_ref,
+                cf::runloop::CFRunLoopGetCurrent(),
+                cf::runloop::kCFRunLoopDefaultMode,
+            );
+
+            let started = FSEventStreamStart(stream_ref);
+            
+            if started {
+                println!("FS Event Stream started!")
+            } else {
+                println!("FS Event Stream failed to start!")
+            }
+
+            println!("running runloop!");
+            cf::runloop::CFRunLoopRun();
+        };
+    });
+
+
+    let (_tx, rx) = get_channel();
+
+    for maybe_string in rx.iter() {
+        if let Some(data) = maybe_string {
+            println!("Event emitted for: {:#?}", data);
+        }
+    };
+}
+
+#[allow(non_snake_case)]
+extern "C" fn callback(
+    _streamRef: fsevent::FSEventStreamRef,
+    _clientCallbackInfo: *mut ffi::c_void,
+    _numEvents: usize,
+    eventPaths: *mut ffi::c_void,
+    _eventFlags: *const u32,
+    _eventIds: *const u64,
+) { 
+    let (tx, _rx) = get_channel();
+
+    let cf_paths: ItemMutRef<CFArray<CFString>> = unsafe { CFArray::from_mut_void(eventPaths) };
+    for path in cf_paths.iter() {
+        tx.send(Some(path.to_string())).unwrap();
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use core_foundation as cf;
+use fsevent_sys as fsevent;
+
+pub type UnsafeMutableRawPointer = *mut std::ffi::c_void;
+
+pub type FSEventStreamRef = *mut ::std::os::raw::c_void;
+
+pub type FSEventStreamCallback = Option<extern "C" fn(
+    streamRef: FSEventStreamRef,
+    clientCallBackInfo: UnsafeMutableRawPointer,
+    numEvents: usize,
+    eventPaths: UnsafeMutableRawPointer,
+    eventFlags: *const fsevent::FSEventStreamEventFlags,
+    eventIds: *const fsevent::FSEventStreamEventId,
+)>;
+
+#[link(name = "CoreServices", kind = "framework")]
+extern "C" {
+    pub fn FSEventStreamCreate(
+        allocator: cf::base::CFAllocatorRef,
+        callback: FSEventStreamCallback,
+        context: UnsafeMutableRawPointer,
+        pathsToWatch: cf::array::CFArrayRef,
+        sinceWhen: fsevent::FSEventStreamEventId,
+        latency: cf::date::CFTimeInterval,
+        flags: fsevent::FSEventStreamEventFlags,
+    ) -> FSEventStreamRef;
+
+    pub fn FSEventStreamScheduleWithRunLoop(
+        streamRef: FSEventStreamRef,
+        runLoop: cf::runloop::CFRunLoopRef,
+        runLoopMode: cf::string::CFStringRef,
+    );
+
+    pub fn FSEventStreamStart(
+        streamRef: FSEventStreamRef,
+    ) -> bool;
+}
